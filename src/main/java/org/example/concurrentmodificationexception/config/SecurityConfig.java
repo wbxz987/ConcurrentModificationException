6bc74d7a284fe3509c0b84520f3c5058fbbe935b
wbxz987 <204740490+wbxz987@users.noreply.github.com>
2025-03-28 11:07:28 +0200

    Add workaround

--- src/main/java/org/example/concurrentmodificationexception/config/SecurityConfig.java
+++ src/main/java/org/example/concurrentmodificationexception/config/SecurityConfig.java
@@ -1,7 +1,20 @@
 package org.example.concurrentmodificationexception.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.security.authorization.method.AuthorizationAdvisor;
+import org.springframework.security.authorization.method.AuthorizationAdvisorProxyFactory;
+import org.springframework.security.authorization.method.AuthorizationManagerAfterMethodInterceptor;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
+import org.springframework.security.authorization.method.AuthorizeReturnObjectMethodInterceptor;
+import org.springframework.security.authorization.method.PostFilterAuthorizationMethodInterceptor;
+import org.springframework.security.authorization.method.PreFilterAuthorizationMethodInterceptor;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +32,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+  @Primary
+  @Bean
+  @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+  static AuthorizationAdvisorProxyFactory proxyFactory() {
+    AuthorizationAdvisorProxyFactory proxyFactory =
+        new AuthorizationAdvisorProxyFactory(new ArrayList<>());
+    List<AuthorizationAdvisor> advisors = new ArrayList<>();
+
+    advisors.add(AuthorizationManagerBeforeMethodInterceptor.preAuthorize());
+    advisors.add(AuthorizationManagerAfterMethodInterceptor.postAuthorize());
+    advisors.add(new PreFilterAuthorizationMethodInterceptor());
+    advisors.add(new PostFilterAuthorizationMethodInterceptor());
+    advisors.add(new AuthorizeReturnObjectMethodInterceptor(proxyFactory));
+
+    AnnotationAwareOrderComparator.sort(advisors);
+
+    for (AuthorizationAdvisor advisor : advisors) {
+      proxyFactory.addAdvisor(advisor);
+    }
+
+    return proxyFactory;
+  }
+
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.httpBasic(Customizer.withDefaults())
